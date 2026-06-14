4a855a3f6abb34768669a5ffb2de80cb5e836b3b
Aleksandr Linkov <deimoslink@yandex.ru>
2026-06-14 18:06:02 +0400

    Updates

--- custom-sw.js
+++ custom-sw.js
@@ -4,40 +4,32 @@ const INDEX_URL = BASE_PATH + 'index.html';
 (function () {
   'use strict';
 
-  // Это событие срабатывает, когда Service Worker успешно активирован и готов к работе
-  self.addEventListener('activate', (event) => {
-    console.log('[Custom SW] Service Worker успешно инициализирован и активирован.');
-  });
-
-  // Лог для подтверждения загрузки файла
-  console.log('[Custom SW] Файл sw.js успешно загружен и применил importScripts для ngsw-worker.js');
-
-
   self.addEventListener('fetch', (event) => {
-
     if (event.request.mode === 'navigate') {
       console.log('[SW] Fetching navigation:', event.request.url);
       event.respondWith(
         fetch(event.request)
           .then(response => {
+            // Если сеть вернула 404, идем в кэш
             if (!response || response.status === 404) {
               throw new Error('404');
             }
             return response;
           })
-          .catch(() => {
+          .catch(async () => {
             console.log('[SW] Network failed/404, serving cached index.html');
-            return caches.match(INDEX_URL);
+            const cachedResponse = await caches.match(INDEX_URL);
+            return cachedResponse || fetch(INDEX_URL);
           })
       );
       return;
     }
 
+    // Для остальных запросов
     event.respondWith(
       fetch(event.request).catch(() => caches.match(event.request))
     );
   });
-
 })();
 
 // Подключаем стандартный Angular Service Worker
