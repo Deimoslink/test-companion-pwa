98f5d289039a98138e150cd358399db74b64633a
Aleksandr Linkov <deimoslink@yandex.ru>
2026-06-14 18:16:41 +0400

    service worker import order changed

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,23 +1,10 @@
-// Подключаем стандартный Angular Service Worker
-importScripts('./ngsw-worker.js');
-
 const BASE_PATH = '/test-companion-pwa/';
 const INDEX_URL = BASE_PATH + 'index.html';
 
 (function () {
   'use strict';
 
-  // Это событие срабатывает, когда Service Worker успешно активирован и готов к работе
-  self.addEventListener('activate', (event) => {
-    console.log('[Custom SW] Service Worker успешно инициализирован и активирован.');
-  });
-
-  // Лог для подтверждения загрузки файла
-  console.log('[Custom SW] Файл sw.js успешно загружен и применил importScripts для ngsw-worker.js');
-
-
   self.addEventListener('fetch', (event) => {
-
     if (event.request.mode === 'navigate') {
       console.log('[SW] Fetching navigation:', event.request.url);
       event.respondWith(
@@ -28,9 +15,10 @@ const INDEX_URL = BASE_PATH + 'index.html';
             }
             return response;
           })
-          .catch(() => {
+          .catch(async () => {
             console.log('[SW] Network failed/404, serving cached index.html');
-            return caches.match(INDEX_URL);
+            const cachedResponse = await caches.match(INDEX_URL);
+            return cachedResponse || fetch(INDEX_URL);
           })
       );
       return;
@@ -40,8 +28,10 @@ const INDEX_URL = BASE_PATH + 'index.html';
       fetch(event.request).catch(() => caches.match(event.request))
     );
   });
-
 })();
+
+// Connect generic angular SW
+importScripts('./ngsw-worker.js');
 
 
 
