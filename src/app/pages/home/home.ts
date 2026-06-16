91ced2f74cc1b7de784dc291c88a88f3d473fdaa
Aleksandr Linkov <deimoslink@yandex.ru>
2026-06-16 13:23:07 +0400

    scripts fix

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { ApiService } from '@core/services/api.service';
 import { toSignal } from '@angular/core/rxjs-interop';
